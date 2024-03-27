26e2435f36a7284bc29dbade570e87c574865586
Nil Xie <bfgh2678@gmail.com>
2024-03-27 20:16:33 +0800

    create Rect

--- basic-variables/Rect.playground/Contents.swift
+++ basic-variables/Rect.playground/Contents.swift
@@ -3,3 +3,5 @@ import UIKit
 let thePoint = CGPoint(x: 10, y: 10)
 let thsSize = CGSize(width: 60, height: 80)
 let theRect = CGRect(origin: thePoint, size: thsSize)
+let theView = UIView(frame: theRect)
+theView.backgroundColor = UIColor.green
