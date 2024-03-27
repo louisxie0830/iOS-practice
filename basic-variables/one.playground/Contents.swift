d17bb24c07bb520fcf76f9b33ebfc02760ad402e
Nil Xie <bfgh2678@gmail.com>
2024-03-27 10:58:58 +0800

    basic varables

---
+++ basic-variables/one.playground/Contents.swift
@@ -0,0 +1,21 @@
+// let 常數
+let a = 1
+let b:Int = 2
+let c:Int = a + b
+print(c)
+
+// var 變數
+var d:Int = 1
+var e:Int = 2
+var f:Int = d + e
+print(f)
+f = f + 1
+print(f)
+
+// Int and Double
+var g = 1.0
+var h = 2
+var i = Int(g) + h
+print(i)
+
+print(g > 2)
